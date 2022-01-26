7569c94d406eb4687e6c5ae51594c7b92ffe5852
dmschd0678@naver.com <63108606+dmschd0678@users.noreply.github.com>
2022-01-27 06:48:58 +0900

    second commit

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,17 @@ from xml.etree.ElementInclude import include
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('',views.index, name = 'index')
+    # ex : /polls
+    path('',views.index, name = 'index'),
+    
+    # ex : /polls/5/
+    path('<int:question_id>/', views.detail, name = 'detail'),
+    
+    # ex : /polls/5/result/
+    path('<int:qustion_id>/result', views.results, name = 'results'),
+    
+    # ex : /polls/5/vote/
+    path('<int:question_id>/vote', views.vote, name = 'vote') 
 ]
