c676d1b223c5b9db0e606dce7e7353d29c59fbe3
dmschd0678@naver.com <63108606+dmschd0678@users.noreply.github.com>
2022-01-29 03:56:57 +0900

    오류 수정 및 django part 4

--- polls/urls.py
+++ polls/urls.py
@@ -1,17 +1,28 @@
-from xml.etree.ElementInclude import include
 from django.urls import path
 from . import views
 
 app_name = 'polls'
 urlpatterns = [
+    # # ex : /polls
+    # path('',views.index, name = 'index'),
+    
+    # # ex : /polls/6/
+    # path('<int:question_id>/', views.detail, name = 'detail'),
+    
+    # # ex : /polls/5/result/
+    # path('<int:question_id>/results', views.results, name = 'results'),
+    
+    # # ex : /polls/5/vote/
+    # path('<int:question_id>/vote', views.vote, name = 'vote') 
+
     # ex : /polls
-    path('',views.index, name = 'index'),
+    path('',views.IndexView.as_view(), name = 'index'),
     
     # ex : /polls/6/
-    path('<int:question_id>/', views.detail, name = 'detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name = 'detail'),
     
     # ex : /polls/5/result/
-    path('<int:qustion_id>/results', views.results, name = 'results'),
+    path('<int:pk>/results', views.ResultsView.as_view(), name = 'results'),
     
     # ex : /polls/5/vote/
     path('<int:question_id>/vote', views.vote, name = 'vote') 
